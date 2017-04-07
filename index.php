6bcf3e6e42e38c7e8ae192e78dd483b341701ab6
matugo <matugoag98@gmail.com>
2017-04-06 19:32:27 -0500

    Create index.php
    formulario en donde se llama la función por medio de un cuadro de texto.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!--
+Autor= Marcos Gonzalez
+Teg: ADSI
+-->
+<html>
+<head>
+	<title>formuario</title>
+</head>
+<body>
+	<form action="detectar_si_existe_mysqli.php" method="GET">
+	<input type="text" name="funcion">
+	<input type="submit" value="verifiar">	
+</form>
+</body>
+</html>
